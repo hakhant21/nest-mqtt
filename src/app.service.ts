ea47a03a0f48fc2b777ee8dc66326cbdf5e6854a
Htet Aung Khant <htetaungkhant@securelinkmm.com>
2024-10-28 11:02:14 +0630

    nest mqtt

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { MqttClient, connect } from 'mqtt';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleInit, OnModuleDestroy {
+  client: MqttClient;
+
+  onModuleInit() {
+    this.connect();
+  }
+  private connect() {
+    this.client = connect(`${process.env.MQTT_URL}`);
+
+    this.client.on('connect', () => {
+      console.log('Connected to MQTT broker');
+      this.subscribe('#');
+    });
+  }
+
+  private subscribe(topic: string) {
+      this.client.subscribe(topic, (error) => {
+        if (error) {
+          console.log(`Failed to subscribe to topic ${topic}: ${error.message}`);
+        } else {
+          console.log(`Subscribed to topic: ${topic}`);
+        }
+      })
+
+      this.client.on('message', (topic, message) => {
+        console.log(`Received message on topic ${topic}: ${message.toString()}`);
+      });
+  }
+
+  public publish(topic: string, message: string) {
+    this.client.publish(topic, message, (error) => {
+      if (error) {
+        console.log(`Failed to publish to topic ${topic}: ${error.message}`);
+      } else {
+        console.log(`Published message to topic ${topic}: ${message}`);
+      }
+    });
+  }
+
+  onModuleDestroy() {
+    if (this.client) {
+      this.client.end();
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  
+  const mqttOptions: MicroserviceOptions = {
+    transport: Transport.MQTT,
+    options: {
+      url: process.env.MQTT_URL
+    },
+  };
+  app.connectMicroservice(mqttOptions);
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
